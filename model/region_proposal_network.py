d877def11f65343acb52302cb8569b2bb7caa44e
qxcheng <qxchenger>
2020-01-06 09:36:54 +0800

    add net comments

--- model/region_proposal_network.py
+++ model/region_proposal_network.py
@@ -8,24 +8,8 @@ from model.utils.creator_tool import ProposalCreator
 
 
 class RegionProposalNetwork(nn.Module):
-    """
-        in_channels (int): The channel size of input.
-        mid_channels (int): The channel size of the intermediate tensor.
-        ratios (list of floats): This is ratios of width to height of the anchors.
-        anchor_scales (list of numbers): This is areas of anchors.
-            Those areas will be the product of the square of an element in
-            :obj:`anchor_scales` and the original area of the reference
-            window.
-        feat_stride (int): Stride size after extracting features from an image.
-        initialW (callable): Initial weight value. If :obj:`None` then this
-            function uses Gaussian distribution scaled by 0.1 to
-            initialize weight.
-            May also be a callable that takes an array and edits its values.
-        proposal_creator_params (dict): Key valued paramters for
-            :class:`model.utils.creator_tools.ProposalCreator`.
-
-    """
-
+    # ratios: width/height
+    # anchor_scales: areas of anchors
     def __init__(self, in_channels=512, mid_channels=512, ratios=[0.5, 1, 2],
                  anchor_scales=[8, 16, 32], feat_stride=16, proposal_creator_params=dict(),):
         super(RegionProposalNetwork, self).__init__()
@@ -37,47 +21,15 @@ class RegionProposalNetwork(nn.Module):
         self.conv1 = nn.Conv2d(in_channels, mid_channels, 3, 1, 1)
         self.score = nn.Conv2d(mid_channels, n_anchor * 2, 1, 1, 0)
         self.loc = nn.Conv2d(mid_channels, n_anchor * 4, 1, 1, 0)
-
         normal_init(self.conv1, 0, 0.01)
         normal_init(self.score, 0, 0.01)
         normal_init(self.loc, 0, 0.01)
 
     def forward(self, x, img_size, scale=1.):
-        """
-        * :math:`N` is batch size.
-        * :math:`C` channel size of the input.
-        * :math:`H` and :math:`W` are height and witdh of the input feature.
-        * :math:`A` is number of anchors assigned to each pixel.
-
-        Args:
-            x (~torch.autograd.Variable): The Features extracted from images.
-                Its shape is :math:`(N, C, H, W)`.
-            img_size (tuple of ints): A tuple :obj:`height, width`,
-                which contains image size after scaling.
-            scale (float): The amount of scaling done to the input images after
-                reading them from files.
-
-        Returns:
-            (~torch.autograd.Variable, ~torch.autograd.Variable, array, array, array):
-
-            * **rpn_locs**: Predicted bounding box offsets and scales for \
-                anchors. Its shape is :math:`(N, H W A, 4)`.
-            * **rpn_scores**:  Predicted foreground scores for \
-                anchors. Its shape is :math:`(N, H W A, 2)`.
-            * **rois**: A bounding box array containing coordinates of \
-                proposal boxes.  This is a concatenation of bounding box \
-                arrays from multiple images in the batch. \
-                Its shape is :math:`(R', 4)`. Given :math:`R_i` predicted \
-                bounding boxes from the :math:`i` th image, \
-                :math:`R' = \\sum _{i=1} ^ N R_i`.
-            * **roi_indices**: An array containing indices of images to \
-                which RoIs correspond to. Its shape is :math:`(R',)`.
-            * **anchor**: Coordinates of enumerated shifted anchors. \
-                Its shape is :math:`(H W A, 4)`.
-
-        """
+        # img_size: [600,800]
+        # scale: float=1.6=600/375=800/500
         n, _, hh, ww = x.shape                                                                   # [1, 512, 37, 50]
-        anchor = _enumerate_shifted_anchor(np.array(self.anchor_base), self.feat_stride, hh, ww) # (16650, 4)
+        anchor = _enumerate_shifted_anchor(np.array(self.anchor_base), self.feat_stride, hh, ww) # (16650, 4)    返回值
 
         n_anchor = anchor.shape[0] // (hh * ww)                                                  # int=9
         h = F.relu(self.conv1(x))                                                                # [1, 512, 37, 50]
@@ -85,22 +37,23 @@ class RegionProposalNetwork(nn.Module):
         rpn_locs = self.loc(h)                                                                   # [1, 36, 37, 50]
         # UNNOTE: check whether need contiguous
         # A: Yes
-        rpn_locs = rpn_locs.permute(0, 2, 3, 1).contiguous().view(n, -1, 4)                      # [1,16650,4]
+        rpn_locs = rpn_locs.permute(0, 2, 3, 1).contiguous().view(n, -1, 4)                      # [1,16650,4]   返回值
         rpn_scores = self.score(h)                                                               # [1,18,37,50]
         rpn_scores = rpn_scores.permute(0, 2, 3, 1).contiguous()                                 # [1,37,50,18]
         rpn_softmax_scores = F.softmax(rpn_scores.view(n, hh, ww, n_anchor, 2), dim=4)           # [1,37,50,9,2]
         rpn_fg_scores = rpn_softmax_scores[:, :, :, :, 1].contiguous()                           # [1,37,50,9]
         rpn_fg_scores = rpn_fg_scores.view(n, -1)                                                # [1,16650]
-        rpn_scores = rpn_scores.view(n, -1, 2)                                                   # [1,16650,2]
+        rpn_scores = rpn_scores.view(n, -1, 2)                                                   # [1,16650,2]   返回值
 
         rois = list()
         roi_indices = list()
         for i in range(n):
             roi = self.proposal_layer(
-                rpn_locs[i].cpu().data.numpy(),
-                rpn_fg_scores[i].cpu().data.numpy(),
-                anchor, img_size,
-                scale=scale)
+                rpn_locs[i].cpu().data.numpy(),       # [16650,4]
+                rpn_fg_scores[i].cpu().data.numpy(),  # [16650]
+                anchor,                               # (16650, 4)
+                img_size,                             # [600,800]
+                scale=scale)                          # float=1.6
             batch_index = i * np.ones((len(roi),), dtype=np.int32)
             rois.append(roi)
             roi_indices.append(batch_index)
